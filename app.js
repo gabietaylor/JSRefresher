3af4d2c07e070f958086160699f461d75960f7fe
gabietaylor <gabie.taylor@yahoo.com>
2017-10-13 14:07:21 -0400

    math operators logging

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ console.log('Namaste'); // string
 console.log(1234); // number
 console.log(true); // boolean
 console.log(null); // null
+
+console.log(99 + 1); // add
+console.log(99 - 1); // subtract
+console.log(99 * 1); // multiply
+console.log(99 / 1); // divide
