9af9103d0ad669899b5cae82441802cbf801f909
gabietaylor <gabie.taylor@yahoo.com>
2017-10-15 11:23:14 -0400

    commented code out

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ console.log(99 - 1); // subtract
 console.log(99 * 1); // multiply
 console.log(99 / 1); // divide
 
-console.log("boom whats up".length);
+// console.log("boom whats up".length);
 
-console.log('namaste'.toUpperCase());
+/* console.log('namaste'.toUpperCase());
 console.log('    Remove whitespace   '.trim());
 console.log('boolean'.startsWith('g'));
 console.log('boolean'.endsWith('n'));
-
+ */
 console.log(Math.random() * 25);
 console.log(Math.floor(Math.random() * 25));
 console.log(Math.ceil(43.8));
