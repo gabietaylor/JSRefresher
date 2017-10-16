df1dc0611c03107af4d91f15e0cf1d5d6243c8b4
gabietaylor <gabie.taylor@yahoo.com>
2017-10-16 16:39:24 -0400

    added undefined example

--- twoapp.js
+++ twoapp.js
@@ -4,8 +4,14 @@ const price = 3;
 console.log(breakfast);
 console.log(price);
 
+// can be changed
 let change = true;
 console.log(change);
 
 change = false;
 console.log(change);
+
+// undefined example
+// anything not given a = value
+let undefined;
+console.log(undefined);
